debf3106124bb25b806787854b5524b2fdbe4dd5
jaeweon <wowon7975@naver.com>
2018-05-10 23:06:41 +0900

    User model 올바른 지정

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.conf import settings
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL) #Bad case
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.user
